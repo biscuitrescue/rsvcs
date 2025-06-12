cb65e6640945716d77d394d91277e3ddc06a5574
Karttikeya Sinha <biscuitrescue>
2025-06-12 22:22:25 +0530

    `rsvcs add` working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{fs::{create_dir_all, write, read}, path::Path};
+use std::{fs::{create_dir_all, write, read}, path::Path, io::Write};
 use clap::{Parser, Subcommand};
-// use sha1::{Sha1, Digest};
-use sha1::Sha1;
+use sha1::{Sha1, Digest};
 
 #[derive(Parser)] // auto impl trait
 #[command(name = "rsvcs")]
@@ -49,10 +48,28 @@ fn add_file(file: &str) {
     let contents = read(path)
         .expect("Failed to read {file}");
 
+    // Computes hash
     let mut hasher = Sha1::new();
     hasher.update(&contents);
+    let hash = hex::encode(hasher.finalize());
 
-    // let hash = hex::encode(hasher.finalize);
+    let obj_path = Path::new(".rsvcs").join("commits").join(&hash);
+    
+    if !obj_path.exists() {
+        write(&obj_path, &contents)
+            .expect("Failed to write blob");
+    }
+
+    let mut index = std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(".rsvcs/index")
+        .expect("Failed to open index!");
+
+    writeln!(index, "{} {}", hash, file)
+        .expect("Failed to append to index");
+
+    println!("Added file: {} (hash: {})", file, hash);
 }
 
 fn commit(message: &str) {
