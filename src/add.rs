c04123900ef9c7cddda6903f2de0c214af09913c
Karttikeya Sinha <biscuitrescue>
2025-06-16 01:28:40 +0530

    fixed add

--- src/add.rs
+++ src/add.rs
@@ -1,5 +1,5 @@
 use sha1::{Sha1, Digest};
-use std::{path::Path, io::Write, fs::{read, write}};
+use std::{fs::{create_dir_all, read, write}, io::Write, path::Path};
 
 pub fn add_file(file: &str) {
     let path = Path::new(file);
@@ -10,14 +10,20 @@ pub fn add_file(file: &str) {
     }
 
     let contents = read(path)
-        .expect("Failed to read {file}");
+        .expect("Failed to read contents");
 
     // Computes hash
     let mut hasher = Sha1::new();
     hasher.update(&contents);
     let hash = hex::encode(hasher.finalize());
 
-    let obj_path = Path::new(".rsvcs").join("commits").join(&hash);
+    let obj_dir = Path::new(".rsvcs/objects");
+    if !obj_dir.exists() {
+        create_dir_all(obj_dir)
+            .expect("Failed to create {obj_dir}");
+    }
+    
+    let obj_path = obj_dir.join(&hash);
     if !obj_path.exists() {
         write(&obj_path, &contents)
             .expect("Failed to write blob");
--- src/commit.rs
+++ src/commit.rs
@@ -1,13 +1,13 @@
 // use chrono::{Local, DateTime, Utc};
 use sha1::{Sha1, Digest};
 use serde::{Serialize, Deserialize};
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, fs::{remove_file, write, read_to_string}, path::Path};
 
 #[allow(dead_code)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Commit {
     pub message: String,
-    pub files,
+    pub files: HashMap<String, String>,
     // pub timestamp: DateTime<Utc>
 }
 
@@ -25,7 +25,7 @@ pub fn commit_changes(message: &str) {
     let mut files = HashMap::new();
 
     for line in content.lines() {
-        let val: Vec<&str> = line.split_whitespaces().collect();
+        let val: Vec<&str> = line.split_whitespace().collect();
         if val.len() == 2 {
             files.insert(val[1].to_string(), val[0].to_string());
         }
@@ -39,5 +39,22 @@ pub fn commit_changes(message: &str) {
     };
 
     // hashing left
-}
+    let serial = serde_json::to_string_pretty(&commit)
+        .expect("Failed to serialise");
 
+    let mut hasher = Sha1::new();
+    hasher.update(&serial);
+    let hash = hex::encode(hasher.finalize());
+
+    let c_path = Path::new("./.rsvcs/commits").join(&hash);
+    write(&c_path, serial)
+        .expect("Failed to write commit");
+
+    write(".rsvcs/HEAD", &hash)
+        .expect("Failed to update HEAD");
+
+    remove_file(ind_path)
+        .expect("Failed to clear index");
+
+    println!("Committed as {}: {}", &hash[..7], message);
+}
